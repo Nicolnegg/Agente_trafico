84bc7765b73179157a0e653cba1085137616857e
Nicolnegg <princesnegg@gmail.com>
2023-03-20 21:38:42 -0500

    final

--- agente.py
+++ agente.py
@@ -5,6 +5,7 @@ from scapy.all import *
 import requests
 import time
 import random
+import paramiko
 
 
 def generate_http_traffic():
@@ -21,60 +22,79 @@ def generate_http_traffic():
     http_request.show()
     sendp(http_request)
 
+    #filtro http
+
 
 def generate_https_traffic():
 
-    fake = Faker()
-    url = "https://" + fake.domain_name()
-    response = requests.get('https://www.google.com', verify=True)
-    print(response.content)
-    
+    url = fake.url()
+    print(url)
+    try:
+        response = requests.get(url)
+    except requests.exceptions.RequestException as e:
+        print("Error al realizar la solicitud HTTPS:", e)
+    #filtro tls.handshake.type == 1 and tcp.port == 443 and ssl.handshake.extensions_server_name == "www.example.com"
     
 def generate_ssh_traffic():
     # Crear una solicitud SSH
-    ssh_request = scapy.all.Ether()/scapy.all.IP(dst="10.0.0.2")/scapy.all.TCP(dport=22, flags="S")
-    ssh_request.show()
-    scapy.all.sendp(ssh_request)
+    # Crea una sesión SSH utilizando la biblioteca Paramiko
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect('us01.vpnhack.xyz', username='vpnhack-root', password='123')
+    # enviar comando a través de la conexión SSH
+    stdin, stdout, stderr = ssh.exec_command('ls')
+    # imprimir salida del comando
+    print(stdout.read())
+    # cerrar la conexión SSH
+    ssh.close()
+    #filtro ssh
 
 def generate_mail_traffic():
     # Crear una solicitud de correo electrónico
     dst_ip = fake.ipv4_private()
     mail_request = scapy.all.Ether()/scapy.all.IP(dst=dst_ip)/scapy.all.TCP(dport=25)/scapy.all.Raw(load="HELO example.com\r\nMAIL FROM: user@example.com\r\nRCPT TO: user2@example.com\r\nDATA\r\nSubject: prueba de correo electrónico\r\nEste es un mensaje de prueba.\r\n.\r\nQUIT\r\n")
     scapy.all.sendp(mail_request)
+     #filtro smtp
 
 def generate_ftp_traffic():
     # Crear una solicitud FTP
     dst_ip = fake.ipv4_private()
     ftp_request = scapy.all.Ether()/scapy.all.IP(dst=dst_ip)/scapy.all.TCP(dport=21)/scapy.all.Raw(load="USER anonymous\r\nPASS anonymous\r\nLIST\r\nQUIT\r\n")
     scapy.all.sendp(ftp_request)
+     #filtro ftp
 
 def generate_traffic():
     # Generar tráfico aleatorio
-    while True:
-        traffic_type = random.randint(1, 5)
-        if traffic_type == 1:
-            print("HTTP")
-            generate_http_traffic()            
-        # elif traffic_type == 2:
-        #     print("HTTPS")
-        #     generate_https_traffic()
-        #     
-        # elif traffic_type == 3:
-        #     generate_ssh_traffic()
-        #     print("SSH")
-        # # elif traffic_type == 4:
-        #     generate_mail_traffic()
-        # elif traffic_type == 5:
-        #     generate_ftp_traffic()
-
-        # Esperar un tiempo aleatorio antes de generar el siguiente tráfico
-
-fake = Faker()
-if __name__ == "__main__":
-    traffic_interval = 10 # tiempo entre cada generación de tráfico, en segundos
-    while True:       
-        
+    traffic_type = random.randint(1, 5)
+    if traffic_type == 1:
+        print("HTTP")
+        generate_http_traffic()            
+    elif traffic_type == 2:
+        print("HTTPS")
         generate_https_traffic()
-        time.sleep(traffic_interval) 
-        print("HTTPs")
+            
+    elif traffic_type == 3:
+        print("SSH")
+        generate_ssh_traffic()
+            
+    elif traffic_type == 4:
+        print("SMTP")
+        generate_mail_traffic()
+    elif traffic_type == 5:
+        print("FTP")
+        generate_ftp_traffic()
+
         
+fake = Faker()
+
+# Tiempo de ejecución deseado en segundos
+tiempo_deseado = 120
+
+# Tiempo de inicio
+tiempo_inicio = time.time()
+if __name__ == "__main__":
+    traffic_interval = 0 # tiempo entre cada generación de tráfico, en segundos
+    while time.time() - tiempo_inicio < tiempo_deseado:       
+        
+        generate_traffic()
+        time.sleep(traffic_interval) 
