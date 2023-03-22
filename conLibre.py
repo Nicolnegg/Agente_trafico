fc8b519ff04ddd131bc9990d7288e91c3ae4105f
Nicolnegg <princesnegg@gmail.com>
2023-03-21 22:08:26 -0500

    esto

--- conLibre.py
+++
@@ -1,9 +0,0 @@
-import paramiko
-
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect("192.168.1.112", username="user",
-            key_filename=os.path.join(os.path.expanduser('~'), ".ssh", "id_ed25519"))
-stdin, stdout, stderr = ssh.exec_command('ls -l')
-print(stdout.read().decode())
-ssh.close()
